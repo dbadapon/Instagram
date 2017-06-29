062b96dd43f95f31185691b8d52a4f1f865ea8b2
Dominique Adapon <dadapon@fb.com>
2017-06-29 13:49:08 -0700

    Can see detail view from profile

--- Instagram/ProfileViewController.swift
+++ Instagram/ProfileViewController.swift
@@ -87,14 +87,19 @@ class ProfileViewController: UIViewController, UICollectionViewDataSource {
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
+        let cell = sender as! UICollectionViewCell
+        if let indexPath = profileCollectionView.indexPath(for: cell) {
+            let post = allPosts[indexPath.row]
+            let detailViewController = segue.destination as! PostDetailsViewController
+            
+            detailViewController.post = post
+            
+        }
+    }
 }
